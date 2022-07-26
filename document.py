511d56d84c3518a57a2c4ef26313f071aac6ec87
shubham0204 <equipintelligence@gmail.com>
2022-07-26 17:43:57 +0530

    Improved algorithm and added documentation

--- document.py
+++ document.py
@@ -1,13 +1,32 @@
 import numpy as np
-import base64
 import cv2
 
-class DocumentScanner:
+# Returns the coordinates ( x , y , w , h ) for the bounding box of the document in the given
+# image. This image processing algorithm has a number of limitations like:
+# 1. It requires high contrast between the document and the background to detect the edges
+# 2. The document should be contained within the image only ( the four vertices of the document must be present
+#    in the input image )
+def get_rect( img ):
 
-    def __init__( self , image_bytes ):
-        self.__image_bytes = image_bytes
+    # Erosion of the image - Unwanted pixel noise is eliminated by eroding the image,
+    # thereby enhancing the document-background borders.
+    kernel = np.ones( (7, 7), np.uint8)
+    img = cv2.erode( img , kernel , iterations=1 )
+    
+    # Colorspace conversion - For our use-case, we don't require any color information as such ( this leads us to
+    # first limitation though ). It is also necessary as we're performing contour detection in further steps.
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    def __convert_image_bytes(self):
-        base64.b64decode( self.__image_bytes )
+    #
+    img = cv2.GaussianBlur(img, (9, 9), 0)
+    img = cv2.Canny(img, 75, 200)
+    contours, _ = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contour_perimeters = [cv2.arcLength(contour, True) for contour in contours]
+    doc_contour = contours[np.argmax(contour_perimeters)]
+    x, y, w, h = cv2.boundingRect(doc_contour)
+    return x, y, w, h
 
+def get_binarized_img( img ):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return cv2.threshold( img , 150 , 255 , cv2.THRESH_BINARY )[ 1 ]
 
---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI , File , UploadFile
+from fastapi.responses import Response
+from document import get_rect , get_binarized_img
+import numpy as np
+import cv2
+
+app = FastAPI()
+
+# POST method to get the rect of the cropped document
+# It requires an `image` in the body of the request
+# FastAPI docs : 1. https://fastapi.tiangolo.com/tutorial/body
+#                2. https://fastapi.tiangolo.com/tutorial/request-files
+@app.post( "/get_rect" )
+async def show_image( image : UploadFile = File() ):
+    contents = await image.read()
+    # Converting the `contents` bytes to an OpenCV Mat
+    # Refer this SO answer -> https://stackoverflow.com/a/61345230/13546426
+    img = cv2.imdecode( np.fromstring( contents, np.uint8 ), cv2.IMREAD_COLOR)
+    rect = get_rect( img )
+    return rect
+
+
+@app.post( "/binarize" )
+async def binarize( image : UploadFile = File() ):
+    contents = await image.read()
+    img = cv2.imdecode(np.fromstring(contents, np.uint8), cv2.IMREAD_COLOR)
+    img = get_binarized_img( img )
+    img_bytes = cv2.imencode('.png', img )[1].tobytes()
+    return Response( img_bytes , media_type='image/png' )
